4218686a8331ef30b8c088d7ff6c625177127b47
JayMCook <jmcook5520@gmail.com>
2025-10-05 16:04:15 -0400

    small naming error fixed

--- lab1-server-threading.py
+++ lab1-server-threading.py
@@ -34,7 +34,7 @@ def link_handler(link, client, id):
                             chat_key = tuple(sorted([id, recieve_id]))
                             if chat_key not in chat_logs:
                                 chat_logs[chat_key] = []
-                            chat_logs[chat_key].append(f"{id}: {msg}")
+                            chat_logs[chat_key].append(f"{id}: {message}")
 
                             client_list[recieve_id].sendall(
                                 f"Message from {id}: {message}".encode()
